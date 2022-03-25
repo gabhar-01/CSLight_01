b101d44841a9cec58a99ec161763ed4f0d1edf8a
gabhar-01 <shevchenko.lilya@mail.ru>
2022-03-25 22:32:25 +0700

    Create CSLight_04.cs

---
+++ CSLight_04.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSLight_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userPicsCount = 52;
+            int picsInTheRow = 3;
+            int fullRows, remainingPics;
+            fullRows = userPicsCount / picsInTheRow;
+            remainingPics = userPicsCount % picsInTheRow;
+            Console.WriteLine("Выведется " + fullRows + " полностью заполненных рядов картинок, а так же " + remainingPics + " картинка сверх меры.");
+        }
+    }
+}
