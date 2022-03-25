e47caf04041c0743c07e7926b42e1e399a5755fe
gabhar-01 <shevchenko.lilya@mail.ru>
2022-03-25 21:30:01 +0700

    CSLight_01

---
+++ CSLight_01.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CSLight_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Хидэо", surname = "Кодзима";
+            char gender = 'м';
+            int age, birthDay = 24, birthMonth = 8, birthYear = 1963;
+            float money = 100500.5f, charityDonation;
+            bool isTheyGenius = true;
+        }
+    }
+}
---
+++ CSLight_03.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CSLight_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+            string zodiacSign;
+            string job;
+
+            Console.Write("Как вас зовут: ");
+            name = Console.ReadLine();
+            Console.Write("Сколько вам лет: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Какой ваш знак зодиака: ");
+            zodiacSign = Console.ReadLine();
+            Console.Write("Где вы работаете: ");
+            job = Console.ReadLine();
+
+            Console.WriteLine($"Вас зовут {name}, вам {age} год, вы {zodiacSign} и работаете {job}.");
+        }
+    }
+}
