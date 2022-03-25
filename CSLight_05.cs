9ac938108f0c59dc4868b7ccaf3c8b818361d2a7
gabhar-01 <shevchenko.lilya@mail.ru>
2022-03-25 23:05:43 +0700

    Create CSLight_05.cs

---
+++ CSLight_05.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CSLight_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Иванов";
+            string surname = "Иван";
+            string buffer;
+
+            Console.WriteLine("Значения переменных до перестановки:");
+            Console.WriteLine("name: " + name);
+            Console.WriteLine("surname: " + surname);
+            Console.WriteLine();
+
+            buffer = name;
+            name = surname;
+            surname = buffer;
+
+            Console.WriteLine("Значения переменных после перестановки:");
+            Console.WriteLine("name: " + name);
+            Console.WriteLine("surname: " + surname);
+        }
+    }
+}
