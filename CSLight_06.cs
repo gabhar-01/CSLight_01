84e35725a025278023bf32c9cafafc86385a04ea
gabhar-01 <shevchenko.lilya@mail.ru>
2022-03-25 23:34:03 +0700

    Create CSLight_06.cs

---
+++ CSLight_06.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CSLight_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int money;
+            int crystalCount;
+            int crystalPrice = 50;
+
+            Console.Write("Введите количество золота, имеющееся в вашем кошельке: ");
+            money = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Добро пожаловать в Лавку Алхимика! Сегодня кристаллы стоят " + crystalPrice + " золота.");
+            Console.Write("Сколько кристаллов Вы хотите купить: ");
+            crystalCount = Convert.ToInt32(Console.ReadLine());
+
+            money -= crystalCount * crystalPrice;
+
+            Console.WriteLine("Вы купили " + crystalCount + " кристаллов. У вас осталось " + money + " золота.");
+        }
+    }
+}
