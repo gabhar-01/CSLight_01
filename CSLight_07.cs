a6a63c18a371ea2e7aaf73d78ada458a375ebc2c
gabhar-01 <shevchenko.lilya@mail.ru>
2022-03-26 00:27:29 +0700

    Update CSLight_07.cs

--- CSLight_07.cs
+++ CSLight_07.cs
@@ -7,19 +7,19 @@ namespace CSLight_07
         static void Main(string[] args)
         {
             int countOfPeople;
-            int visitingTime = 10;
-            int waitingTime, waitingHour;
+            int visitingTime = 10, hour = 60;
+            int waitingTime, waitingMinute, waitingHour;
 
             Console.WriteLine("Вы заходите в поликлинику и видите огромную очередь из старушек!");
             Console.Write("Введите количество людей в очереди: ");
             countOfPeople = Convert.ToInt32(Console.ReadLine());
 
             waitingTime = countOfPeople * visitingTime;
-            waitingHour = waitingTime / 60;
-            waitingTime = waitingTime % 60;
+            waitingHour = waitingTime / hour;
+            waitingMinute = waitingTime % hour;
 
             Console.WriteLine();
-            Console.WriteLine("Вы должны отстоять в очереди " + waitingHour + " часа " + waitingTime + " минут.");
+            Console.WriteLine("Вы должны отстоять в очереди " + waitingHour + " часа " + waitingMinute + " минут.");
         }
     }
 }
