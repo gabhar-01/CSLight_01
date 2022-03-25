f655eaa066e47fbeba689bba90581f6face90348
gabhar-01 <shevchenko.lilya@mail.ru>
2022-03-26 00:00:41 +0700

    Create CSLight_07.cs

---
+++ CSLight_07.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CSLight_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfPeople;
+            int visitingTime = 10;
+            int waitingTime, waitingHour;
+
+            Console.WriteLine("Вы заходите в поликлинику и видите огромную очередь из старушек!");
+            Console.Write("Введите количество людей в очереди: ");
+            countOfPeople = Convert.ToInt32(Console.ReadLine());
+
+            waitingTime = countOfPeople * visitingTime;
+            waitingHour = waitingTime / 60;
+            waitingTime = waitingTime % 60;
+
+            Console.WriteLine();
+            Console.WriteLine("Вы должны отстоять в очереди " + waitingHour + " часа " + waitingTime + " минут.");
+        }
+    }
+}
